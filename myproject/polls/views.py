ade8b5b69a180c1d2882f385f3f288d11885049e
daidoan774 <75259385+daidoan774@users.noreply.github.com>
2023-04-26 10:38:33 +0700

    Demo Django bai1

---
+++ myproject/polls/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello")
+
+def ham1(request):
+    return HttpResponse("<h1>dem</h1>")
+
+    
